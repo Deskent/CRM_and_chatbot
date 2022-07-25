32d836809a2e2707bdc1f87e8e06826f8cc5f921
Владимр Леонтьев <dllreg.exe@gmail.com>
2022-07-25 12:34:57 +0800

    add services, endpoints

--- crm_django/app_api/urls.py
+++ crm_django/app_api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from app_api.views import GetWorksheetsViewSet
+from app_api.views import (
+    GetWorksheetsViewSet, GetTextsViewSet, GetCategoriesViewSet, SetWorksheetsViewSet)
 
 urlpatterns = [
     path('get_worksheet/', GetWorksheetsViewSet.as_view(), name='get_worksheet'),
-    path('send_worksheet/', GetWorksheetsViewSet.as_view(), name='send_worksheet'),
-    path('get_categories/', GetWorksheetsViewSet.as_view(), name='get_categories'),
-    path('get_texts/', GetWorksheetsViewSet.as_view(), name='get_texts'),
+    path('send_worksheet/', SetWorksheetsViewSet.as_view(), name='send_worksheet'),
+    path('get_categories/', GetCategoriesViewSet.as_view(), name='get_categories'),
+    path('get_texts/', GetTextsViewSet.as_view(), name='get_texts'),
 ]
--- crm_django/app_api/views.py
+++ crm_django/app_api/views.py
@@ -1,9 +1,20 @@
+from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.generics import views, ListAPIView, GenericAPIView
+from django.views import View
+from rest_framework.generics import (
+    views, ListAPIView, GenericAPIView, RetrieveAPIView, CreateAPIView)
 from app_api.serializers import (
-    CategorySerializerModel, ClientSerializerModel, AllIfoSerializerModel, AllIfoSerializerModelAlt)
-from app_api.models import Client, Category, Order
+    CategorySerializerModel,
+    ClientSerializerModel,
+    AllIfoSerializerModel,
+    AllIfoSerializerModelAlt,
+    TextsSerializerModel, SetOrderSerializerModel,
+)
+from app_api.models import Client, Category, Order, Texts
+from app_api.services import DBITexts, DBIClient, DBIOrder
+
+from django.db.models.manager import Manager
 
 
 class GetWorksheetsViewSet(ListAPIView, GenericAPIView):
@@ -13,12 +24,34 @@ class GetWorksheetsViewSet(ListAPIView, GenericAPIView):
     def get(self, request, *args, **kwargs):
         return self.list(request)
 
-#
-# class AddChannelViewSet(CreateAPIView, GenericAPIView):
-#     serializer_class = GetChannelsSerializerModel
-#
-#     def perform_create(self, serializer):
-#         DBIChannel.add_channel(**serializer.data)
-#
-#     def post(self, request, *args, **kwargs):
-#         return self.create(request)
+
+class GetCategoriesViewSet(ListAPIView, GenericAPIView):
+    serializer_class = CategorySerializerModel
+    queryset = Category.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request)
+
+
+class GetTextsViewSet(views.APIView):
+    data = DBITexts.get_texts()
+
+    def get(self, request, *args, **kwargs):
+        data = self.data
+        return JsonResponse(data=data, safe=False)
+
+
+class SetWorksheetsViewSet(CreateAPIView, GenericAPIView):
+    serializer_class = SetOrderSerializerModel
+    queryset = Order
+
+    def perform_create(self, serializer):
+        data = serializer.data
+        client = DBIClient.get_dict(**data)
+        client, client_create = Client.objects.get_or_create(**client)
+        order = DBIOrder.get_dict(**data)
+        order['client_id'] = client.id
+        order, order_create = Order.objects.get_or_create(**order)
+
+    def get(self, request, *args, **kwargs):
+        return self.create(request)
