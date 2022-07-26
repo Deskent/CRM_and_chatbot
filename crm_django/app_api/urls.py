0adcef63d75ced053dadd612afe59f1b0d91753d
Владимр Леонтьев <dllreg.exe@gmail.com>
2022-07-26 19:05:40 +0800

    [build:1.0.12] [version:1.0]
    fix admin panel

--- crm_django/app_api/urls.py
+++ crm_django/app_api/urls.py
@@ -7,6 +7,6 @@ from app_api.views import (
 
 urlpatterns = [
     path('send_worksheet', SetWorksheetsViewSet.as_view(), name='send_worksheet'),
-    path('get_categories', GetCategoriesViewSet.as_view(), name='get_categories'),
+    path('get_categories/', GetCategoriesViewSet.as_view(), name='get_categories'),
     path('get_poll_by_category/<int:category_id>', GetPollByCategoryViewSet.as_view(), name='get_poll_by_category'),
 ]
